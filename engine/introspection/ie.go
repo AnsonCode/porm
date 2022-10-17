fde05931d02ce0a175754740f78b953c3b042ec0
anson <caoyanbin1993@foxmail.com>
2022-10-17 14:53:51 +0800

    bug:fixed pull bug

--- engine/introspection/ie.go
+++ engine/introspection/ie.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -51,7 +52,89 @@ func (e *IntrospectEngine) Pull(provider, url string) (string, error) {
 		provider, url)
 	startParse := time.Now()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*600)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel() // 读取一行数据后，发送kill信号
+
+	cmd := exec.CommandContext(ctx, e.enginePath)
+
+	pipe, err := cmd.StdinPipe() // 标准输入流
+	if err != nil {
+		return "", fmt.Errorf("introspect engine std in pipe %v", err.Error())
+	}
+	defer pipe.Close()
+	// 构建一个json-rpc 请求参数
+	req := IntrospectRequest{
+		Id:      1,
+		Jsonrpc: "2.0",
+		Method:  "introspect",
+		Params: []map[string]interface{}{
+			{
+				"schema":             string(schema),
+				"compositeTypeDepth": -1,
+			},
+		},
+	}
+
+	data, err := json.Marshal(req)
+	if err != nil {
+		return "", err
+	}
+	// 入参追加到管道中
+	_, err = pipe.Write(append(data, []byte("\n")...))
+	if err != nil {
+		return "", err
+	}
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	// 不阻塞启动
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	reader := bufio.NewReader(stdout)
+
+	// TODO:如果一直堵死在这咋办？
+	//阻塞读取，实时读取输出流中的一行内容
+	line, err2 := reader.ReadString('\n')
+	if err2 != nil || io.EOF == err2 {
+		return "", err2
+	}
+	log.Println(line)
+
+	var response IntrospectResponse
+
+	// 解析响应结果
+	err = json.Unmarshal([]byte(line), &response)
+	if err != nil {
+		return "", err
+	}
+
+	log.Printf("[timing] introspect took %s", time.Since(startParse))
+	if response.Error != nil {
+		return "", fmt.Errorf("introspect error: %s", response.Error.Data.Message)
+	}
+	log.Println("introspect successful")
+
+	dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
+	//dataModel := strings.Replace(response.Result.DataModel, " Bytes", " String", -1)
+	return dataModel, nil
+}
+
+func (e *IntrospectEngine) Pull2(provider, url string) (string, error) {
+	schema := fmt.Sprintf(
+		`datasource db {
+			provider = "%s"
+			url      = "%s"
+		}`,
+		provider, url)
+	startParse := time.Now()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
 	cmd := exec.CommandContext(ctx, e.enginePath)
